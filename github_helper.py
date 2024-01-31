d4d0cb9e29576d16e27286c45c7e29280f4573ab
abhijay-py <67355181+abhijay-py@users.noreply.github.com>
2024-01-30 19:48:32 -0500

    handled streaming history

--- github_helper.py
+++ github_helper.py
@@ -2,10 +2,10 @@ import os
 
 def update_gitignore(folder_name):
     jsonfiles = [i for i in os.listdir(folder_name) if i[-5:] == ".json"]
-    pyfiles = os.listdir("__pycache__")
 
     with open(".gitignore", "w") as f:
         for file in jsonfiles:
             f.write(folder_name+"/"+file+"\n")
-        for file in pyfiles:
-            f.write("__pycache__/"+file+"\n")
+        f.write("__pycache__\n")
+        f.write("testing.py\n")
+        f.write("Spotify_Analysis_Files\n")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import csv
 from urllib.parse import unquote
 from github_helper import *
 
@@ -20,6 +21,16 @@ def convert_local_file(song):
         if len(info) == 4:
             break
     return {"trackName":info[3], "artistName":info[1], "albumName":info[2], "trackUri":info[0]}
+
+def save_to_csv(info, typeChar, output_file, reverse=False):
+    if typeChar == 'll':
+        with open(".\\Spotify_Analysis_Files\\"+output_file+".csv", 'w', encoding="utf8") as f:
+            csv_f = csv.writer(f)
+            if not reverse:
+                csv_f.writerows()
+            else:
+                for i in range(len(info) - 1, -1, -1):
+                    csv_f.writerow(info[i])
 
 
 #ADVANCED HELPERS   
@@ -71,6 +82,36 @@ def adjust_artists(data, typeInfo, file_name):
 
     data[file_name] = artistDict
 
+def clean_streaming_audio(data, file_name):
+    songs = data[file_name]
+    newSongs = []
+
+    for song in songs:
+        newSong = {}
+
+        try:
+            client = unquote(song['user_agent_decrypted'])
+        except:
+            client = None
+        
+        if song['ms_played'] == 0:
+            continue
+
+        coreData = {'trackName':song['master_metadata_track_name'], 'artistName':song['master_metadata_album_artist_name'], 'albumName':song['master_metadata_album_album_name'], 'trackUri':song['spotify_track_uri']}
+        streamData = {'timestamp':song['ts'], 'platform':song['platform'], 'country':song['conn_country'], 'ip':song['ip_addr_decrypted'], 'client':client, 
+                        'offline':song['offline'], 'offline_timestamp':song['offline_timestamp'], 'incognito_mode':song['incognito_mode']}
+        episodeData = {'episode_name':song['episode_name'], 'episode_show':song['episode_show_name'], 'episodeUri':song['spotify_episode_uri']}
+        playData = {'ms_played':song['ms_played'], 'reason_start':song['reason_start'], 'reason_end':song['reason_end'], 'shuffle':song['shuffle'], 'skipped':song['skipped']}
+
+        newSong["main_data"] = coreData
+        newSong["streaming_info"] = streamData
+        newSong["episode_details"] = episodeData
+        newSong["play_info"] = playData
+
+        newSongs.append(newSong)
+
+    data[file_name] = newSongs
+
 
 #NON CORE FUNCTIONS
 
@@ -83,6 +124,64 @@ def get_listening_types(artistDict):
 
     return types
 
+def get_all_time_song_info(data, file_name):
+    total = 0
+    uriTracker = {}
+    reversereference = {}
+    realreference = {}
+
+    for song in data['Streaming_History_Audio']:
+        core_data = song['main_data']
+        play_info = song['play_info']
+
+        uri = core_data['trackUri']
+        name = core_data['trackName']
+        artist = core_data['artistName']
+        album = core_data['albumName']
+        ms_played = play_info['ms_played']
+
+        if song['episode_details']['episode_name']:
+            continue
+
+        total += ms_played
+
+        if name == None or artist == None:
+            continue
+        
+        if name + artist in reversereference.keys():
+            uri = reversereference[name+artist]
+        else:
+            realreference[uri] = (name, artist, album)
+            reversereference[name+artist] = uri
+
+        
+        if uri not in uriTracker.keys():
+            uriTracker[uri] = ms_played
+        else:
+            uriTracker[uri] += ms_played
+
+    sortedStuff = sorted(uriTracker.items(), key=lambda x:x[1])
+    sortedBetter = [(realreference[i][0], j, realreference[i][1], realreference[i][2], i) for i, j in sortedStuff]
+    
+    return sortedBetter, total
+
+def get_all_time_artist_info(data, file_name):
+    songs, total = get_all_time_song_info(data, file_name)
+    artists = {}
+
+    for song in songs:
+        artist = song[2]
+        ms_listened = song[1]
+
+        if artist in artists.keys():
+            artists[artist] += ms_listened
+        else:
+            artists[artist] = ms_listened
+
+    sortedStuff = sorted(artists.items(), key=lambda x:x[1])
+
+    return sortedStuff
+    
 
 #CORE FUNCTIONS
 
@@ -101,7 +200,7 @@ def get_constants():
 
 #Renames Marquee into Artists
 #Get the data from the files (currently combining: streamed_music)
-def extract_data(folder_name, stream_file_names, silent=False):
+def extract_data(folder_name, playlistFile, stream_file_names, silent=False):
     files = [i for i in os.listdir(folder_name) if i[-5:] == ".json"]
     data = {}
     typeInfo = {}
@@ -142,15 +241,25 @@ def extract_data(folder_name, stream_file_names, silent=False):
     except:
         pass
 
+    try:
+        data["Playlists"] = data[playlistFile]
+        typeInfo["Playlists"] = typeInfo[playlistFile]
+
+        del data[playlistFile]
+        del typeInfo[playlistFile]
+    except:
+        pass
+
     return data, typeInfo
 
 #Format the data structures correctly (currently formatting: playlists)
-def raw_data_handling(data, typeInfo, constants):
-    clean_playlists(data, constants['Playlists'])
+def raw_data_handling(data, typeInfo, storageFile=None):
+    clean_playlists(data, 'Playlists')
     adjust_artists(data, typeInfo, "Artists")
+    clean_streaming_audio(data, "Streaming_History_Audio")
 
 #Analyze Data
-def analysis(data, typeInfo):
+def analysis(data, typeInfo, file=None):
     pass
 
 
@@ -158,21 +267,23 @@ def analysis(data, typeInfo):
 def main():
     constants = get_constants()
 
-    streamFiles = [constants["Stream_History_Music"]]
+    streamFiles = [constants["Stream_History_Music"], constants["Stream_History_Video"]]
     spotifyDataFolder = constants["Spotify_Data_Folder"]
     pfile = constants['Playlists']
 
-    data, typeInfo = extract_data(spotifyDataFolder, streamFiles, silent=True)
-    raw_data_handling(data, typeInfo, constants)
+    data, typeInfo = extract_data(spotifyDataFolder, pfile, streamFiles, silent=True)
+    raw_data_handling(data, typeInfo)
+
+    raw_songs, total = get_all_time_song_info(data, constants["Stream_History_Music"])
+    raw_artists = get_all_time_artist_info(data, constants["Stream_History_Music"])
+
+    songs = [(i[0], round(i[1]/60)/1000.0, i[2]) for i in raw_songs]
+    artists = [(i, round(j/60)/1000.0) for i, j in raw_artists]
 
     #Begin Testing
-    listening_types = get_listening_types(data["Artists"])
-    print(listening_types)
-    #print([k for k, v in data["Artists"].items() if v == listening_types[0]])
-    #print(data["Artists"]["Vetrom"])
+    save_to_csv(songs, 'll', 'songs', reverse=True)
+    save_to_csv(artists, 'll', 'artists', reverse=True)
     #End Testing
-
-    
 
     update_gitignore(spotifyDataFolder)
 
